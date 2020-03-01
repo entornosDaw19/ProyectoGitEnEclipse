36a80eb69c5305308266b77d188ab7797714c8b8
entornosDaw19 <usuario@usuario-VirtualBox>
2020-03-01 13:03:10 +0100

    Se añade el parametro del main

--- src/Bienvenido.java
+++ src/Bienvenido.java
@@ -1,4 +1,17 @@
 
 public class Bienvenido {
 
-}
+	public static void main(String[] args) {
+		
+		if (args.length==0)
+		{
+			System.out.println("No hay parametros ");
+		}
+			else
+			{
+				System.out.println("Bienvenido "+args[0]);
+				
+				
+			}
+		}
+	}
